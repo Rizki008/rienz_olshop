fee5a48c9468ea1f792e0115b888e252c001825f
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-05-20 21:42:54 +0700

    menambahkan fitur add kategori

---
+++ application/views/backend/kategori/v_add.php
@@ -0,0 +1,63 @@
+<!-- Single pro tab start-->
+<div class="single-product-tab-area mg-b-30">
+    <!-- Single pro tab review Start-->
+    <div class="single-pro-review-area">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="review-tab-pro-inner">
+                        <ul id="myTab3" class="tab-review-design">
+                            <li class="active"><a href="#description"><i class="icon nalika-edit" aria-hidden="true"></i> <?= $title ?></a></li>
+                        </ul>
+                        <div id="myTabContent" class="tab-content custom-product-edit">
+                            <div class="product-tab-list tab-pane fade active in" id="description">
+                                <?php
+                                //notifikasi form kosong
+                                echo validation_errors('<div class="alert alert-warning alert-dismissible">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+            <h5><i class="icon fas fa-exclamation-triangle"></i>', '</h5></div>');
+
+                                //notifikasi gagal upload gambar
+                                if (isset($error_upload)) {
+                                    echo '<div class="alert alert-warning alert-dismissible">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                <h5><i class="icon fas fa-exclamation-triangle"></i>' . $error_upload . '</h5></div>';
+                                }
+                                echo form_open_multipart('master_produk/add_kategori') ?>
+                                <div class="row">
+                                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                        <div class="review-content-section">
+                                            <div class="input-group mg-b-pro-edt">
+                                                <span class="input-group-addon"><i class="icon nalika-user" aria-hidden="true"></i></span>
+                                                <input type="text" name="nama_kategori" value="<?= set_value('nama_kategori') ?>" class="form-control" placeholder="Nama Kategori">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                        <div class="review-content-section">
+                                            <div class="input-group mg-b-pro-edt">
+                                                <span class="input-group-addon"><i class="icon nalika-user" aria-hidden="true"></i></span>
+                                                <input type="file" class="form-control" name="gambar" id="preview_gambar" name="gambar" placeholder="Gambar">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                        <div class="text-center custom-pro-edt-ds">
+                                            <button type="submit" class="btn btn-ctl-bt waves-effect waves-light m-r-10">Save
+                                            </button>
+                                            <a href="<?= base_url('master_produk/kategori') ?>" class="btn btn-ctl-bt waves-effect waves-light">Kembali
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <?php echo form_close() ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
