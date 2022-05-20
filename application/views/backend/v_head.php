dfea430d3a1ab3c5fc4303f635f5bf969b88da7d
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-05-20 11:13:00 +0700

    mengubah view backend

--- application/views/backend/v_head.php
+++ application/views/backend/v_head.php
@@ -2,64 +2,66 @@
 <html class="no-js" lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Dashboard V.1 | Nalika - Material Admin Template</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- favicon
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Dashboard V.1 | Nalika - Material Admin Template</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- favicon
 		============================================ -->
-    <link rel="shortcut icon" type="image/x-icon" href="<?= base_url() ?>backend/img/favicon.ico">
-    <!-- Google Fonts
+  <link rel="shortcut icon" type="image/x-icon" href="<?= base_url() ?>backend/img/favicon.ico">
+  <!-- Google Fonts
 		============================================ -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
-    <!-- Bootstrap CSS
+  <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
+  <!-- Bootstrap CSS
 		============================================ -->
-    <link rel="stylesheet" href="<?= base_url() ?>backend/css/bootstrap.min.css">
-    <!-- Bootstrap CSS
+  <link rel="stylesheet" href="<?= base_url() ?>backend/css/bootstrap.min.css">
+  <!-- Bootstrap CSS
 		============================================ -->
-    <link rel="stylesheet" href="<?= base_url() ?>backend/css/font-awesome.min.css">
-    <!-- nalika Icon CSS
+  <link rel="stylesheet" href="<?= base_url() ?>backend/css/font-awesome.min.css">
+  <!-- nalika Icon CSS
 		============================================ -->
-    <link rel="stylesheet" href="<?= base_url() ?>backend/css/nalika-icon.css">
-    <!-- owl.carousel CSS
+  <link rel="stylesheet" href="<?= base_url() ?>backend/css/nalika-icon.css">
+  <!-- owl.carousel CSS
 		============================================ -->
-    <link rel="stylesheet" href="<?= base_url() ?>backend/css/owl.carousel.css">
-    <link rel="stylesheet" href="<?= base_url() ?>backend/css/owl.theme.css">
-    <link rel="stylesheet" href="<?= base_url() ?>backend/css/owl.transitions.css">
-    <!-- animate CSS
+  <link rel="stylesheet" href="<?= base_url() ?>backend/css/owl.carousel.css">
+  <link rel="stylesheet" href="<?= base_url() ?>backend/css/owl.theme.css">
+  <link rel="stylesheet" href="<?= base_url() ?>backend/css/owl.transitions.css">
+  <!-- animate CSS
 		============================================ -->
-    <link rel="stylesheet" href="<?= base_url() ?>backend/css/animate.css">
-    <!-- normalize CSS
+  <link rel="stylesheet" href="<?= base_url() ?>backend/css/animate.css">
+  <!-- normalize CSS
 		============================================ -->
-    <link rel="stylesheet" href="<?= base_url() ?>backend/css/normalize.css">
-    <!-- meanmenu icon CSS
+  <link rel="stylesheet" href="<?= base_url() ?>backend/css/normalize.css">
+  <!-- meanmenu icon CSS
 		============================================ -->
-    <link rel="stylesheet" href="<?= base_url() ?>backend/css/meanmenu.min.css">
-    <!-- main CSS
+  <link rel="stylesheet" href="<?= base_url() ?>backend/css/meanmenu.min.css">
+  <!-- main CSS
 		============================================ -->
-    <link rel="stylesheet" href="<?= base_url() ?>backend/css/main.css">
-    <!-- morrisjs CSS
+  <link rel="stylesheet" href="<?= base_url() ?>backend/css/main.css">
+  <!-- morrisjs CSS
 		============================================ -->
-    <link rel="stylesheet" href="<?= base_url() ?>backend/css/morrisjs/morris.css">
-    <!-- mCustomScrollbar CSS
+  <link rel="stylesheet" href="<?= base_url() ?>backend/css/morrisjs/morris.css">
+  <!-- mCustomScrollbar CSS
 		============================================ -->
-    <link rel="stylesheet" href="<?= base_url() ?>backend/css/scrollbar/jquery.mCustomScrollbar.min.css">
-    <!-- metisMenu CSS
+  <link rel="stylesheet" href="<?= base_url() ?>backend/css/scrollbar/jquery.mCustomScrollbar.min.css">
+  <!-- metisMenu CSS
 		============================================ -->
-    <link rel="stylesheet" href="<?= base_url() ?>backend/css/metisMenu/metisMenu.min.css">
-    <link rel="stylesheet" href="<?= base_url() ?>backend/css/metisMenu/metisMenu-vertical.css">
-    <!-- calendar CSS
+  <link rel="stylesheet" href="<?= base_url() ?>backend/css/metisMenu/metisMenu.min.css">
+  <link rel="stylesheet" href="<?= base_url() ?>backend/css/metisMenu/metisMenu-vertical.css">
+  <!-- calendar CSS
 		============================================ -->
-    <link rel="stylesheet" href="<?= base_url() ?>backend/css/calendar/fullcalendar.min.css">
-    <link rel="stylesheet" href="<?= base_url() ?>backend/css/calendar/fullcalendar.print.min.css">
-    <!-- style CSS
+  <link rel="stylesheet" href="<?= base_url() ?>backend/css/calendar/fullcalendar.min.css">
+  <link rel="stylesheet" href="<?= base_url() ?>backend/css/calendar/fullcalendar.print.min.css">
+  <!-- style CSS
 		============================================ -->
-    <link rel="stylesheet" href="<?= base_url() ?>backend/style.css">
-    <!-- responsive CSS
+  <link rel="stylesheet" href="<?= base_url() ?>backend/style.css">
+  <!-- responsive CSS
 		============================================ -->
-    <link rel="stylesheet" href="<?= base_url() ?>backend/css/responsive.css">
-    <!-- modernizr JS
+  <link rel="stylesheet" href="<?= base_url() ?>backend/css/responsive.css">
+  <!-- modernizr JS
 		============================================ -->
-    <script src="<?= base_url() ?>backend/js/vendor/modernizr-2.8.3.min.js"></script>
+  <script src="<?= base_url() ?>backend/js/vendor/modernizr-2.8.3.min.js"></script>
 </head>
+
+<body>
