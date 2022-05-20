14dd2f0f12a75253bc330e0990a310a9ea020b51
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-05-20 09:13:15 +0700

    menambahkan model admin

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -6,12 +6,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Admin extends CI_Controller
 {
 
-    // public function __construct()
-    // {
-    //     parent::__construct();
-    //     //Load Dependencies
-
-    // }
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('m_admin');
+    }
 
     // List all your items
     public function index()
