66361863559034d30922a51eeca1e80c028549b1
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-05-21 12:30:06 +0700

    menambahkan crud produk dan diskon

--- application/models/M_master_produk.php
+++ application/models/M_master_produk.php
@@ -4,7 +4,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class M_master_produk extends CI_Model
 {
-
+    //kategori
     public function kategori()
     {
         $this->db->select('*');
@@ -32,5 +32,65 @@ class M_master_produk extends CI_Model
     {
         $this->db->where('id_kategori', $data['id_kategori']);
         $this->db->delete('kategori');
+    }
+
+    //produk
+    public function produk()
+    {
+        $this->db->select('*');
+        $this->db->from('produk');
+        $this->db->join('kategori', 'produk.id_kategori = kategori.id_kategori', 'left');
+        $this->db->order_by('id_produk', 'desc');
+        return $this->db->get()->result();
+    }
+    public function id_produk()
+    {
+        return $this->db->query('SELECT max(id_produk) as id FROM produk')->row();
+    }
+    public function detail_produk($id_produk)
+    {
+        $this->db->select('*');
+        $this->db->from('produk');
+        $this->db->join('kategori', 'produk.id_kategori = kategori.id_kategori', 'left');
+        $this->db->where('id_produk', $id_produk);
+        return $this->db->get()->row();
+    }
+    public function add_produk($data)
+    {
+        $this->db->insert('produk', $data);
+    }
+    public function edit_produk($data)
+    {
+        $this->db->where('id_produk', $data['id_produk']);
+        $this->db->update('produk', $data);
+    }
+    public function delete_produk($data)
+    {
+        $this->db->where('id_produk', $data['id_produk']);
+        $this->db->delete('produk');
+    }
+
+    //diskon
+    public function diskon()
+    {
+        $this->db->select('*');
+        $this->db->from('diskon');
+        $this->db->join('produk', 'diskon.id_produk = diskon.id_produk', 'left');
+        $this->db->order_by('id_diskon', 'desc');
+        return $this->db->get()->result();
+    }
+    public function add_diskon($data)
+    {
+        $this->db->insert('diskon', $data);
+    }
+    public function edit_diskon($data)
+    {
+        $this->db->where('id_diskon', $data['id_diskon']);
+        $this->db->update('diskon', $data);
+    }
+    public function delete_diskon($data)
+    {
+        $this->db->where('id_diskon', $data['id_diskon']);
+        $this->db->delete('diskon');
     }
 }
