a38eb76c42ba57c83f78e331c92721844430bf86
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-05-22 23:17:38 +0700

    menambahkan authentifikasi login pelanggan

--- application/models/M_auth.php
+++ application/models/M_auth.php
@@ -12,4 +12,15 @@ class M_auth extends CI_Model
         $this->db->where(array('username' => $username, 'password' => $password));
         return $this->db->get()->row();
     }
+    public function register($data)
+    {
+        $this->db->insert('pelanggan', $data);
+    }
+    public function pelanggan_login($email, $password)
+    {
+        $this->db->select('*');
+        $this->db->from('pelanggan');
+        $this->db->where(array('email' => $email, 'password' => $password));
+        return $this->db->get()->row();
+    }
 }
