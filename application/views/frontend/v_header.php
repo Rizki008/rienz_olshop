4f904bcf34bfd776abd21269fcaa6af2c0dd119e
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-05-22 23:19:08 +0700

    mengubah fitur cart, login dan register

--- application/views/frontend/v_header.php
+++ application/views/frontend/v_header.php
@@ -34,43 +34,43 @@
             <div class="row">
                 <div class="col-xl-3 col-lg-2">
                     <div class="header__logo">
-                        <a href="<?= base_url() ?>"><img src="img/logo.png" alt=""></a>
+                        <a href="<?= base_url() ?>"><img src="<?= base_url() ?>frontend/img/logo.png" alt=""></a>
                     </div>
                 </div>
                 <div class="col-xl-6 col-lg-7">
                     <nav class="header__menu">
+                        <?php $kategori = $this->m_home->kategori_produk(); ?>
                         <ul>
                             <li class="active"><a href="<?= base_url() ?>">Home</a></li>
-                            <li><a href="#">Women’s</a></li>
-                            <li><a href="#">Men’s</a></li>
-                            <li><a href="./shop.html">Shop</a></li>
-                            <li><a href="#">Pages</a>
-                                <ul class="dropdown">
-                                    <li><a href="./product-details.html">Product Details</a></li>
-                                    <li><a href="./shop-cart.html">Shop Cart</a></li>
-                                    <li><a href="./checkout.html">Checkout</a></li>
-                                    <li><a href="./blog-details.html">Blog Details</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="./blog.html">Blog</a></li>
-                            <li><a href="./contact.html">Contact</a></li>
+                            <?php foreach ($kategori as $key => $value) { ?>
+                                <li><a href="<?= base_url('home/kategori/' . $value->id_kategori) ?>"><?= $value->nama_kategori ?></a></li>
+                            <?php } ?>
                         </ul>
                     </nav>
                 </div>
                 <div class="col-lg-3">
                     <div class="header__right">
                         <div class="header__right__auth">
-                            <a href="#">Login</a>
-                            <a href="#">Register</a>
+                            <?php if ($this->session->userdata('email') == "") { ?>
+                                <a href="<?= base_url('pelanggan/login') ?>">Login</a>
+                                <a href="<?= base_url('pelanggan/register') ?>">Register</a>
+                            <?php } else { ?>
+                                <a href="#"><?= $this->session->userdata('nama_pelanggan'); ?></a>
+                                <a href="<?= base_url('pelanggan/logout') ?>">Logout</a>
+                            <?php } ?>
                         </div>
+                        <?php $keranjang = $this->cart->contents();
+                        $jml_item = 0;
+                        foreach ($keranjang as $key => $value) {
+                            $jml_item = $jml_item + $value['qty'];
+                        } ?>
                         <ul class="header__right__widget">
-                            <li><span class="icon_search search-switch"></span></li>
-                            <li><a href="#"><span class="icon_heart_alt"></span>
-                                    <div class="tip">2</div>
-                                </a></li>
-                            <li><a href="#"><span class="icon_bag_alt"></span>
-                                    <div class="tip">2</div>
-                                </a></li>
+
+                            <li>
+                                <a href="<?= base_url('belanja') ?>"><span class="icon_bag_alt"></span>
+                                    <div class="tip"><?= $jml_item ?></div>
+                                </a>
+                            </li>
                         </ul>
                     </div>
                 </div>
