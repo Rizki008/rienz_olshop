a08b56a1075363fefec585153a1d0b87d0dd191a
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-05-24 14:37:16 +0700

    mengubah view dan proses transaksi

--- application/controllers/Transaksi.php
+++ application/controllers/Transaksi.php
@@ -7,36 +7,51 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Transaksi extends CI_Controller
 {
 
-    // public function __construct()
-    // {
-    //     parent::__construct();
-    //     //Load Dependencies
-
-    // }
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('m_pesanan_masuk');
+        $this->load->model('m_transaksi');
+    }
 
     // List all your items
     public function index()
     {
         $data = array(
             'title' => 'Data Transaksi Pelanggan',
+            'pesanan' => $this->m_pesanan_masuk->pesanan(),
+            'pesanan_diproses' => $this->m_pesanan_masuk->pesanan_diproses(),
+            'pesanan_dikirim' => $this->m_pesanan_masuk->pesanan_dikirim(),
+            'pesanan_selesai' => $this->m_pesanan_masuk->pesanan_selesai(),
+            'proses_kirim' => $this->m_pesanan_masuk->proses_kirim(),
             'isi' => 'backend/transaksi/v_transaksi'
         );
         $this->load->view('backend/v_wrapper', $data, FALSE);
     }
 
-    // Add a new item
-    public function add()
+    public function detail($no_order)
     {
+        $data = array(
+            'title' => 'Detail Pesanan Pembeli',
+            'pesanan' => $this->m_transaksi->pesanan($no_order),
+            'pesanan_detail' => $this->m_transaksi->pesanan_detail($no_order),
+            'pesanan_diproses' => $this->m_pesanan_masuk->pesanan_diproses(),
+            'proses_kirim' => $this->m_pesanan_masuk->proses_kirim(),
+            'isi' => 'backend/transaksi/v_detail'
+        );
+        $this->load->view('backend/v_wrapper', $data, FALSE);
     }
 
-    //Update one item
-    public function update($id = NULL)
+    public function kirim($id_transaksi)
     {
-    }
-
-    //Delete one item
-    public function delete($id = NULL)
-    {
+        $data = array(
+            'id_transaksi' => $id_transaksi,
+            'no_resi' => $this->input->post('no_resi'),
+            'status_order' => 2
+        );
+        $this->m_pesanan_masuk->update_order($data);
+        $this->session->set_flashdata('pesan', 'Pesanan Berhasil Dikirim');
+        redirect('transaksi');
     }
 }
 
--- application/models/M_transaksi.php
+++ application/models/M_transaksi.php
@@ -17,6 +17,12 @@ class M_transaksi extends CI_Model
         $this->db->insert('rinci_transaksi', $data_rinci);
     }
 
+    public function update_order($data)
+    {
+        $this->db->where('id_transaksi', $data['id_transaksi']);
+        $this->db->update('transaksi', $data);
+    }
+
     public function belum_bayar()
     {
         $this->db->select('*');
@@ -54,6 +60,25 @@ class M_transaksi extends CI_Model
         $this->db->where('id_pelanggan', $this->session->userdata('id_pelanggan'));
         $this->db->where('status_order=3');
         $this->db->order_by('id_transaksi', 'desc');
+        return $this->db->get()->result();
+    }
+
+    public function pesanan($no_order)
+    {
+        $this->db->select('*');
+        $this->db->from('transaksi');
+        $this->db->where('transaksi.no_order', $no_order);
+        return $this->db->get()->result();
+    }
+
+    public function pesanan_detail($no_order)
+    {
+        $this->db->select('*');
+        $this->db->from('transaksi');
+        $this->db->join('rinci_transaksi', 'transaksi.no_order = rinci_transaksi.no_order', 'left');
+        $this->db->join('produk', 'rinci_transaksi.id_produk = produk.id_produk', 'left');
+        $this->db->join('size', 'produk.id_produk = size.id_produk', 'left');
+        $this->db->where('transaksi.no_order', $no_order);
         return $this->db->get()->result();
     }
 }
