1bd1a5b97c4fbdb81d0c87b3981e488ea33041f8
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-08-21 22:22:41 +0700

    menambahkan view analisis pelanggan

---
+++ application/views/pemilik/analisis/v_pelanggan.php
@@ -0,0 +1,59 @@
+<div class="breadcome-area">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="breadcome-list single-page-breadcome">
+                    <div class="row">
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                            <div class="breadcome-heading">
+                                <form role="search" class="">
+                                    <!-- <input type="text" placeholder="Search..." class="form-control"> -->
+                                    <!-- <a href=""><i class="fa fa-search"></i></a> -->
+                                </form>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                            <ul class="breadcome-menu">
+                                <li><a href="#">Home</a> <span class="bread-slash">/</span>
+                                </li>
+                                <li><span class="bread-blod"><?= $title ?></span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Charts Start-->
+<div class="charts-area mg-tb-15">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="charts-single-pro responsive-mg-b-30">
+                    <div class="alert-title">
+                        <h2>Pelanggan Loyal</h2>
+                        <!-- <p>A bar chart provides a way of showing data values. It is sometimes used to show trend data. we create a bar chart for a single dataset and render that in our page.</p> -->
+                    </div>
+                    <div id="bar1-chart">
+                        <canvas id="myChartsa"></canvas>
+
+                        <!-- <canvas id="myChartsa" height="100" style="height: 100px;"></canvas> -->
+                    </div>
+                </div>
+            </div>
+            <!-- <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <div class="charts-single-pro">
+                    <div class="alert-title">
+                        <h2>Bar Chart vertical</h2>
+                        <p>A bar chart provides a way of showing data values represented as vertical bars. It is sometimes used to show trend data, and the comparison of multiple data sets</p>
+                    </div>
+                    <div id="line2-chart">
+                        <canvas id="barchart2"></canvas>
+                    </div>
+                </div>
+            </div> -->
+        </div>
+    </div>
+</div>
