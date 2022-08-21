687a6d79523dc52fc16b35cbd483b98b24ad8322
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-08-21 22:23:44 +0700

    menambahkan mdel analisis pelanggan

--- application/models/M_transaksi.php
+++ application/models/M_transaksi.php
@@ -102,6 +102,21 @@ class M_transaksi extends CI_Model
         $this->db->from('rekening');
         return $this->db->get()->result();
     }
+
+    public function grafik_pelanggan()
+    {
+
+        // return $this->db->query("SELECT COUNT(transaksi.no_order) as qty, nama, month(tgl_order) FROM transaksi JOIN pelanggan ON transaksi.id_pelanggan=pelanggan.id_pelanggan GROUP BY transaksi.id_pelanggan")->result();
+        $this->db->select_sum('qty');
+        $this->db->select('pelanggan.nama_pelanggan');
+        $this->db->select('rinci_transaksi.qty');
+        $this->db->from('rinci_transaksi');
+        $this->db->join('transaksi', 'rinci_transaksi.no_order = transaksi.no_order', 'left');
+        $this->db->join('pelanggan', 'transaksi.id_pelanggan = pelanggan.id_pelanggan', 'left');
+        $this->db->group_by('pelanggan.id_pelanggan');
+        $this->db->order_by('qty', 'desc');
+        return $this->db->get()->result();
+    }
 }
 
 /* End of file M_transaksi.php */
