406edd5817571771d30debf24eab4bbb978a9ae1
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-10-30 12:43:40 +0700

    ubah js

--- application/views/v_cekouth.php
+++ application/views/v_cekouth.php
@@ -490,11 +490,11 @@
 
         $(document).ready(function() {
             $("#column_select").change(function() {
+
                 $("#layout_select")
                     .find("option")
                     .show()
                     .not("option[value*='" + this.value + "']").hide();
-
                 $("#layout_select").val(
                     $("#layout_select").find("option:visible:first").val());
 
